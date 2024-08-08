39b7c8ef54e9fdb6a5b0326fdd6513834642bae8
Ddev456 <dev.delhalle@gmail.com>
2024-08-08 08:36:29 +0200

    add testimonials section

--- app/components/Rooms.tsx
+++ app/components/Rooms.tsx
@@ -1,7 +1,93 @@
-import { Mountain, Star, User, Wifi } from "lucide-react";
+import {
+  BathIcon,
+  Mountain,
+  Star,
+  User,
+  UtensilsCrossed,
+  Wifi,
+} from "lucide-react";
 import Image from "next/image";
+import { cn } from "../lib/utils";
+
+interface Room {
+  id: number;
+  name: string;
+  rating: number;
+  capacity: number;
+  description: string;
+  thumbnail: string;
+  services: Service[];
+}
+
+interface Service {
+  id: number;
+  name: string;
+  icon: React.ReactNode;
+}
 
 export const Rooms = () => {
+  const rooms = [
+    {
+      id: 1,
+      name: "Chambre Lavande",
+      rating: 3.4,
+      capacity: 1,
+      description: "Une suite spacieuse avec vue panoramique sur les champs",
+      thumbnail: "/lavande.jpg",
+      services: [
+        {
+          id: 1,
+          name: "Wifi",
+          icon: <Wifi className="w-6 h-6 stroke-slate-800" />,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "Chambre Olivier",
+      rating: 4.2,
+      capacity: 2,
+      description: "Idéal pour un couple",
+      thumbnail: "/olivier.jpg",
+      services: [
+        {
+          id: 1,
+          name: "Wifi",
+          icon: <Wifi className="w-6 h-6 stroke-slate-800" />,
+        },
+        {
+          id: 2,
+          name: "Petit Déjeuner inclus",
+          icon: <UtensilsCrossed className="w-6 h-6 stroke-slate-800" />,
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "Le Romarin",
+      rating: 4.7,
+      capacity: 4,
+      description: "Grande suite adaptée aux familles avec enfants",
+      thumbnail: "/lavande.jpg",
+      services: [
+        {
+          id: 1,
+          name: "Wifi",
+          icon: <Wifi className="w-6 h-6 stroke-slate-800" />,
+        },
+        {
+          id: 2,
+          name: "Petit Déjeuner inclus",
+          icon: <UtensilsCrossed className="w-6 h-6 stroke-slate-800" />,
+        },
+        {
+          id: 3,
+          name: "Salle de bain privative",
+          icon: <BathIcon className="w-6 h-6 stroke-slate-800" />,
+        },
+      ],
+    },
+  ];
   return (
     <div className="flex flex-col items-center gap-8 text-slate-900 md:p-[4rem]">
       <div className="flex flex-col items-start gap-4 p-4">
@@ -11,80 +97,78 @@ export const Rooms = () => {
           conviennent le mieux pour séjourner dans la Provence.
         </span>
       </div>
-      <div className="flex flex-col gap-4">
-        <div className="card flex flex-col md:flex-row text-xl font-bold items-center gap-4 p-6">
-          <div className="flex flex-col gap-16 w-[28rem]">
-            <div className="badge flex justify-between gap-2 p-3 bg-slate-300">
-              <span>4.4</span>
-              <div className="flex gap-1">
-                <Star className="w-6 h-6 stroke-slate-800 fill-slate-800" />
-                <Star className="w-6 h-6 stroke-slate-800 fill-slate-800" />
-                <Star className="w-6 h-6 stroke-slate-800 fill-slate-800" />
-                <Star className="w-6 h-6 stroke-slate-800 fill-slate-800" />
-              </div>
-            </div>
-            <h3>Chambre Lavande</h3>
-            <div className="flex items-center gap-3 p-1 font-medium text-slate-600">
-              <User className="w-6 h-6 stroke-slate-800 fill-slate-800" />
-              <span>1 - 2 personnes</span>
-            </div>
+      <div className="flex flex-col gap-[10rem]">
+        {rooms.map((room) => (
+          <RoomCard room={room} key={room.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-            <div className="flex items-center gap-3 p-1 font-medium text-slate-600">
-              <Mountain className="w-6 h-6 stroke-slate-800 fill-slate-800" />
-              <span>
-                Une suite spacieuse avec vue panoramique sur les champs
-              </span>
-            </div>
-            <div className="flex items-center gap-3 p-1 font-medium text-slate-600">
-              <Wifi className="w-6 h-6 stroke-slate-800" />
-              <span>Wifi disponible</span>
-            </div>
-            <div className="cta flex gap-3 p-1">
-              <button className="text-white bg-slate-700 hover:scale-[0.96] text-[14px] font-semibold transition-transform px-4 py-4">
-                <span className="text-center">Découvrir la Chambre</span>
-              </button>
-              <button className="nav-parent text-slate-900 bg-slate-50 border-2 border-slate-900 hover:scale-[0.96] text-[14px] font-semibold transition-transform px-4 py-4">
-                <span className="nav-link text-center">
-                  Contacter l&apos;hôte
-                </span>
-              </button>
-            </div>
+export const RoomCard = ({ room }: { room: Room }) => {
+  return (
+    <div
+      className={cn(
+        room.id === 3 ? "bg-slate-300" : "",
+        "card flex flex-col md:flex-row text-xl font-bold items-center gap-4 p-6"
+      )}
+    >
+      <div
+        className={cn(
+          room.id % 2 === 0 ? `order-${room.id}` : "order-last",
+          "flex flex-col justify-between w-[28rem] h-[400px]"
+        )}
+      >
+        <div className="badge flex justify-between gap-2 p-3 bg-slate-300">
+          <span>{room.rating}</span>
+          <div className="flex gap-1">
+            {Array(Math.round(room.rating))
+              .fill(0)
+              .map((_, i) => (
+                <Star
+                  key={room.id + i}
+                  className="w-6 h-6 stroke-slate-800 fill-slate-800"
+                />
+              ))}
           </div>
-          <div className="card-bg-lavande w-[500px] h-[600px] rounded-lg" />
+        </div>
+        <h3>{room.name}</h3>
+        <div className="flex items-center gap-3 p-1 font-medium text-slate-600">
+          <User className="w-6 h-6 stroke-slate-800 fill-slate-800" />
+          <span>{room.capacity} personnes</span>
         </div>
 
-        <div className="card flex flex-col md:flex-row-reverse text-xl font-bold items-center gap-4 p-6">
-          <div className="flex flex-col gap-4 w-72">
-            <h3>Chambre Olivier</h3>
-            <span className="font-medium">
-              Chambre double cosy avec terrasse privée
-            </span>
-          </div>
-          <Image
-            src="/olivier.jpg"
-            alt="room2"
-            width={400}
-            height={300}
-            className="rounded-lg shadow-xl"
-          />
+        <div className="flex items-center gap-3 p-1 font-medium text-slate-600">
+          <Mountain className="w-6 h-6 stroke-slate-800 fill-slate-800" />
+          <span>{room.description}</span>
         </div>
-
-        <div className="card w-full p-6 gap-4 flex flex-col font-bold text-xl justify-center">
-          <div className="flex flex-col gap-4">
-            <h3>Le Romarin</h3>
-            <span className="font-medium">
-              Chambre familiale avec espace enfants
-            </span>
+        {room.services.map((service) => (
+          <div
+            className="flex items-center gap-3 p-1 font-medium text-slate-600"
+            key={service.id}
+          >
+            {service.icon}
+            <span>{service.name}</span>
           </div>
-          <Image
-            src="/lavande.jpg"
-            alt="room3"
-            width={600}
-            height={400}
-            className="rounded-lg shadow-xl"
-          />
+        ))}
+
+        <div className="cta flex gap-3 p-1">
+          <button className="text-white bg-slate-700 hover:scale-[0.96] text-[14px] font-semibold transition-transform px-4 py-4">
+            <span className="text-center">Découvrir la Chambre</span>
+          </button>
+          <button className="nav-parent text-slate-700 bg-slate-50 border-2 border-slate-600 hover:scale-[0.96] text-[14px] font-semibold transition-transform px-4 py-4">
+            <span className="nav-link text-center">Réserver</span>
+          </button>
         </div>
       </div>
+      <Image
+        src={room.thumbnail}
+        alt="room3"
+        width={600}
+        height={400}
+        className="rounded-lg shadow-xl"
+      />
     </div>
   );
 };
--- app/components/Testimonials.tsx
+++ app/components/Testimonials.tsx
@@ -5,44 +5,75 @@ import { InfiniteMovingCards } from "@/app/components/ui/infinite-moving-cards";
 
 export function Testimonials() {
   return (
-    <div className="h-[40rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
-      <InfiniteMovingCards
-        items={testimonials}
-        direction="right"
-        speed="normal"
-      />
-    </div>
+    <>
+      <div className="w-full flex flex-col items-start gap-4 p-4 md:p-[4rem]">
+        <h1 className="text-xl font-bold">Les témoignages</h1>
+        <span className="text-2xl">
+          Parce qu&apos;ils en parlent mieux que nous !
+        </span>
+      </div>
+
+      <div className="mt-[-12rem] h-[30rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="right"
+          speed="slow"
+        />
+      </div>
+    </>
   );
 }
 
 const testimonials = [
   {
-    quote:
-      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
+    quote: "C'était un séjour inoubliable, la piscine est un vrai plus !",
     name: "Charles Dickens",
-    title: "A Tale of Two Cities",
+    title: "Je recommande !",
   },
   {
     quote:
-      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+      "Nous avons séjourner du 12 au 19 août, la vue est magnifique, le petit-déjeuner servi dans la chambre est un détail très appréciable ! Je recommanderai cette adesse à mes amis et ma famille.",
     name: "William Shakespeare",
-    title: "Hamlet",
+    title: "Séjour mémorable",
   },
   {
-    quote: "All that we see or seem is but a dream within a dream.",
+    quote:
+      "Une petite maison d'hôtes mais avec un service irréprochable et de qualité. Les hôtes sont très sympas, l'environnement est agréable.",
     name: "Edgar Allan Poe",
-    title: "A Dream Within a Dream",
+    title: "A refaire",
+  },
+  {
+    quote: "C'était un séjour inoubliable, la piscine est un vrai plus !",
+    name: "Charles Dickens",
+    title: "Je recommande !",
   },
   {
     quote:
-      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-    name: "Jane Austen",
-    title: "Pride and Prejudice",
+      "Nous avons séjourner du 12 au 19 août, la vue est magnifique, le petit-déjeuner servi dans la chambre est un détail très appréciable ! Je recommanderai cette adesse à mes amis et ma famille.",
+    name: "William Shakespeare",
+    title: "Séjour mémorable",
   },
   {
     quote:
-      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-    name: "Herman Melville",
-    title: "Moby-Dick",
+      "Une petite maison d'hôtes mais avec un service irréprochable et de qualité. Les hôtes sont très sympas, l'environnement est agréable.",
+    name: "Edgar Allan Poe",
+    title: "A refaire",
+  },
+  {
+    quote: "C'était un séjour inoubliable, la piscine est un vrai plus !",
+    name: "Charles Dickens",
+    title: "Je recommande !",
+  },
+  {
+    quote:
+      "Nous avons séjourner du 12 au 19 août, la vue est magnifique, le petit-déjeuner servi dans la chambre est un détail très appréciable ! Je recommanderai cette adesse à mes amis et ma famille.",
+    name: "William Shakespeare",
+    title: "Séjour mémorable",
+  },
+  {
+    quote:
+      "Une petite maison d'hôtes mais avec un service irréprochable et de qualité. Les hôtes sont très sympas, l'environnement est agréable.",
+    name: "Edgar Allan Poe",
+    title: "A refaire",
   },
 ];
